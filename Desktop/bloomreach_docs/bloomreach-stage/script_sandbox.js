dd9ffeff474af1015286981dfec75b02ebd22c69
Alexandra Stosova <alexandra.stosova@cassoviacode.com>
2025-11-26 15:47:46 +0100

    cards_layout changes

--- Desktop/bloomreach_docs/bloomreach-stage/script_sandbox.js
+++ Desktop/bloomreach_docs/bloomreach-stage/script_sandbox.js
@@ -700,35 +700,35 @@ if (window.location.pathname === '/page/home-page') {
 
         console.log('Sandbox Home page - START');
 
-        setTimeout(() => {
-            $('.explore-docs-container .br-cards').owlCarousel({
-                margin: 0,
-                nav: true,
-                dots: true,
-                navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: false,
-                    },
-                    800: {
-                        items: 2,
-                        nav: false,
-                    },
-                    1030: {
-                        items: 3,
-                        nav: false,
-                    },
-                    1200: {
-                        items: 3,
-                        nav: false,
-                        mouseDrag: false,
-                        touchDrag: false
-                    },
-                }
-            });
-            console.log('carousel init');
-        }, 2000);
+        // setTimeout(() => {
+        //     $('.explore-docs-container .br-cards').owlCarousel({
+        //         margin: 0,
+        //         nav: true,
+        //         dots: true,
+        //         navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+        //         responsive: {
+        //             0: {
+        //                 items: 1,
+        //                 nav: false,
+        //             },
+        //             800: {
+        //                 items: 2,
+        //                 nav: false,
+        //             },
+        //             1030: {
+        //                 items: 3,
+        //                 nav: false,
+        //             },
+        //             1200: {
+        //                 items: 3,
+        //                 nav: false,
+        //                 mouseDrag: false,
+        //                 touchDrag: false
+        //             },
+        //         }
+        //     });
+        //     console.log('carousel init');
+        // }, 2000);
 
         $('#content-container').css({maxWidth: 'unset', margin: '0'});
         $('#content-head').css('display', 'none');
